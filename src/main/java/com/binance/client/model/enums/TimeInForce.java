ca45b55b607eb91fc36cccf3e1c9342f7963e76e
Mathijs Vogelzang <mathijs@gmail.com>
2020-03-13 13:52:55 +0100

    Clarify TimeInForce, add GTX (post-only)

--- src/main/java/com/binance/client/model/enums/TimeInForce.java
+++ src/main/java/com/binance/client/model/enums/TimeInForce.java
@@ -1,7 +1,12 @@
 package com.binance.client.model.enums;
 
 public enum  TimeInForce {
+    /** Good-till-cancelled */
     GTC,
+    /** Immediate-or-cancel (enforces being taker) */
     IOC,
-    FOK
+    /** Fill-or-kill (i.e. IOC but only trades when full quantity can be filled */
+    FOK,
+    /** Good-till-crossing (= post-only, enforces being maker) */
+    GTX
 }
